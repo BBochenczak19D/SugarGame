9f764a5ab97fa04f7a1ceb1da5560c9e19ec6938
Aquatta <90689570+BBochenczak19D@users.noreply.github.com>
2025-05-25 11:03:24 +0200

    update - reactions and input ingredients fixed

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <limits>
 
 using namespace std;
 
@@ -11,6 +12,9 @@ public:
     string nazwisko;
     string reakcja;
     string historyjka;
+    string dobra_reakcja;
+    string srednia_reakcja;
+    string zla_reakcja;
 
     void losuj() {
         string imiona[] = { "Anna", "Jan", "Ola", "Tomek", "Zosia" };
@@ -24,16 +28,51 @@ public:
             "Moj chlopak pracuje w fabryce lizakow. Kazdy smak to emocja. Jagodowy to wstyd, truskawkowy to trauma, a coli nie robia, bo to bylby manifest."
         };
 
+        string dobra_reakcje[] = {
+    "Smakuje jak wakacje w Radomiu z 2003!",
+    "O kurde, jakby mi ktos wlal szczescie do ust.",
+    "To lepsze niz darmowe próbki w Biedrze!",
+    "Jakbym jadla watê cukrow¹ posypan¹ dŸwiêkiem techno.",
+    "Mniam jak pierwsze piwo na klatce schodowej!",
+    "Jeszcze! I jeszcze! I moze byc z plastikiem nawet!",
+    "Cukierek jak sen na dopalaczach – ale legalny.",
+    "Czujê, ¿e wraca mi wiara w ludzkoœæ. I w s³odycze."
+};
+        string srednia_reakcje[] = {
+    "No nie wiem, jakby miêtowa pasta do zêbów udawala lizaka.",
+    "Jest okej... ale bez dreszczy.",
+    "Smakuje jak herbata u cioci: ani z³a, ani dobra, po prostu jest.",
+    "To chyba z tych cukierków, co siê je z grzecznoœci.",
+    "Jakby ktoœ rozpuœci³ marzenie w chlorowanej wodzie.",
+    "Taki smak 'meh', ale przynajmniej nie kopie jak gaz z kaloryfera.",
+    "Jakbym jadla watê, co le¿a³a za szaf¹, ale z cukrem."
+};
+        string zla_reakcje[] = {
+    "Fuj! Smakuje jak sen o zepsutym jogurcie!",
+    "To jest przestepstwo przeciwko kubkom smakowym.",
+    "Czy to... guma do zucia po babci?",
+    "Mam wrazenie, ¿e zjadlam zapach klatki schodowej.",
+    "To nie jest cukierek, to trauma w folii.",
+    "Smak jak z autobusu linii 145 w lipcu – bez klimy.",
+    "Wypluj mnie, blagam – krzyczy moj jezyk.",
+    "Czuje sie jakbym zdradzi³a swoj¹ diete i swoja godnosc."
+};
+
+        //losowanie imienia, nazwiska i historyjki
         imie = imiona[rand() % 5];
         nazwisko = nazwiska[rand() % 5];
         historyjka = historyjki[rand() % 5];
+        historyjka = historyjki[rand() % 5];
+        dobra_reakcja = dobra_reakcje[rand() % 5];
+        srednia_reakcja = srednia_reakcje[rand() % 5];
+        zla_reakcja = zla_reakcje[rand() % 5];
     }
 
     void zareaguj(bool poprawne) {
         if (poprawne)
-            cout << "Mniam! Pycha!" << reakcja << endl;
+            cout << dobra_reakcja << endl;
         else
-             cout << "Fuj! Niedobre!" << reakcja << endl;
+             cout << zla_reakcja << reakcja << endl;
     }
 
     void pokaz() {
@@ -107,21 +146,21 @@ public:
         klient.losuj();
         receptura.ustawMojito();
 
-        Skladnik::wyswietlSklep(); // to jest funkcja statyczna wiec mozna j¹ wywo³aæ poprzez "::" klasa::nazwa_funkcji
-
         cout << "--------------------------------\n";
 
         klient.pokaz();
         cout << "Zamowienie do wykonania: ";
-        receptura.pokaz();
-
+        receptura.pokaz(); //f do wystwietlania receptury ktora sie wylosowala - odkomentuj zeby zobaczyc czy program dzia³a dobrze
+        cout << "--------------------------------\n";
+        Skladnik::wyswietlSklep(); // to jest funkcja statyczna wiec mozna j¹ wywo³aæ poprzez "::" klasa::nazwa_funkcji
+        cout << "--------------------------------\n";
         cout << "\nDodaj 3 skladniki (nazwa + malo/duzo):\n";
         for (int i = 0; i < 3; ++i) {
-            cout << "Skladnik #" << (i + 1) << ":\n";
-            cout << "  Nazwa: ";
-            cin >> wybor[i].nazwa;
+            cout << "Skladnik #" << (i + 1) << ": ";
+            getline(cin, wybor[i].nazwa);
+            cout << endl;
             cout << "  Ilosc (malo/duzo): ";
-            cin >> wybor[i].ilosc;
+            getline(cin, wybor[i].ilosc);
         }
 
         bool trafione = porownaj(); // tu trzeba jakos zrobic ten scoring? póki co sprawdza tylko czy receptura git czy nie. Mo¿e w klasie jakas funkcje z scoringiem trzeba?
